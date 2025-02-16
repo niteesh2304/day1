c54c79801228cc01f012a828c13127c7096acd95
niteesh2304 <svniteesh2004@gmail.com>
2025-02-16 14:47:23 +0530

    Initial commit

--- hw6.java
+++ hw6.java
@@ -7,7 +7,24 @@ import java.util.Scanner;
 //  DA = 90% Basic Salary > 20000 : HRA = 30%, DA = 95%
 
 public class hw6{
+   public static void main(String args[]){
     Scanner sc = new Scanner(System.in);
+    
+    int basic_Salary = sc.nextInt();
+    double gross_salary;
 
+    if (basic_Salary<=10000){
+        gross_salary = basic_Salary + basic_Salary * 0.2 + basic_Salary * 0.8;
+        System.out.println("Gross salary "+gross_salary);
+    }else if(basic_Salary<=20000){
+        gross_salary = basic_Salary + basic_Salary * 0.25 + basic_Salary * 0.9;
+        System.out.println("Gross salary "+gross_salary); 
+    }else if(basic_Salary > 20000){
+        gross_salary = basic_Salary + basic_Salary*0.3 + basic_Salary*0.95;
+        System.out.println("Gross salary "+gross_salary);
+    }else{
+        System.out.println("null");
+    }
+}
     
 }
